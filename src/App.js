6444701ada9aa3c0e42ba67d126c45dca73f69a5
Gustavo Sousa <gustavohl98@gmail.com>
2021-06-21 15:17:58 -0300

    Exercise 1 finished - change from Redux to Context API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.moveCar = this.moveCar.bind(this);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        [car]: side
+      },
+    })
+  }
+
+  render() {
+    const contextValue = {
+      cars: this.state.cars,
+      moveCar: this.moveCar,
+    }
+
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
